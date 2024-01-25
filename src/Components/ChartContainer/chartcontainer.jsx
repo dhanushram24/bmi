6c2873fa51be06970eb4bc690dd7b0518e5c9b8c
dhanushram24 <guttuladhanush.ram@vitstudent.ac.in>
2024-01-25 21:41:11 +0530

    Fixed code

--- src/Components/ChartContainer/chartcontainer.jsx
+++ src/Components/ChartContainer/chartcontainer.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from 'react';
-import styles from './ChartContainer.module.css';
+import styles from './chartcontainer.module.css';
 import { Chart } from 'chart.js/auto';
 
 
